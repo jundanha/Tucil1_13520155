13effa3d854a990df210a5f86f6e6ad636dcf003
Jundan Haris <jundanha@gmail.com>
2022-01-20 14:22:42 +0700

    .

--- file.c
+++ file.c
@@ -2,4 +2,4 @@
 // NIM         : 13520155
 // Tanggal     : Kamis, 20 Januari 2022
 // Mata Kuliah : IF2211 Strategi Algoritma
-// Deskripsi   : Tugas Kecil 1 Penyelesaian Word Search Puzzle dengan Algoritma Brute Force
+// Deskripsi   : Tugas Kecil 1 Penyelesaian Word Search Puzzle dengan Algoritma Brute Force 
